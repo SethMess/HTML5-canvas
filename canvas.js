e61f3f4fcb5e1f70277d1bef6a556fe2dac34002
SethMess <sethwmessier@gmail.com>
2025-01-12 22:09:24 -0800

    added bouncing circle

--- canvas.js
+++ canvas.js
@@ -5,40 +5,83 @@ canvas.height = window.innerHeight;
 
 let c = canvas.getContext('2d');
 
-c.fillStyle = "rgba(255, 0, 0, 0.5)";
-c.fillRect(100, 100, 100, 100);
-c.fillStyle = "rgba(0, 255, 0, 0.5)";
-c.fillRect(300, 100, 100, 100);
-c.fillStyle = "rgba(0, 0, 255, 0.5)";
-c.fillRect(100, 300, 100, 100);
-c.fillStyle = "rgba(255, 0, 255, 0.5)";
-c.fillRect(400, 400, 100, 100);
+// c.fillStyle = "rgba(255, 0, 0, 0.5)";
+// c.fillRect(100, 100, 100, 100);
+// c.fillStyle = "rgba(0, 255, 0, 0.5)";
+// c.fillRect(300, 100, 100, 100);
+// c.fillStyle = "rgba(0, 0, 255, 0.5)";
+// c.fillRect(100, 300, 100, 100);
+// c.fillStyle = "rgba(255, 0, 255, 0.5)";
+// c.fillRect(400, 400, 100, 100);
 
 
 
-c.beginPath
-c.moveTo(50, 300);
-c.lineTo(300, 100);
-c.lineTo(400, 300);
-c.strokeStyle = "red";
-c.lineTo(100, 500);
-// c.strokeStyle = "blue";
-c.stroke();
+// c.beginPath
+// c.moveTo(50, 300);
+// c.lineTo(300, 100);
+// c.lineTo(400, 300);
+// c.strokeStyle = "red";
+// c.lineTo(100, 500);
+// // c.strokeStyle = "blue";
+// c.stroke();
 
-//arc size
-for(var i = 0; i < 2000; i++){
-    var x = Math.random() * window.innerWidth;
-    var y = Math.random() * window.innerHeight;
+// //arc size
+// for (var i = 0; i < 2000; i++) {
+//     var x = Math.random() * window.innerWidth;
+//     var y = Math.random() * window.innerHeight;
+//     c.beginPath();
+//     c.arc(x, y, 15, 0, Math.PI * 3, false);
+//     c.strokeStyle = make_random_colour();
+//     c.lineWidth = 30;
+//     c.stroke();
+
+// }
+
+let x = Math.random() * innerWidth;
+let y = Math.random() * innerHeight;
+let dx = (Math.random() - 0.5) * 8;
+let dy = (Math.random() - 0.5) * 8;
+let radius = 30;
+let colour = make_random_colour();
+
+function animate() {
+
+    // console.log('hi');
+    c.clearRect(0, 0, innerWidth, innerHeight);
+
     c.beginPath();
-    c.arc(x, y, 30, 0, Math.PI*3, false);
-    c.strokeStyle = make_random_colour();
-    c.lineWidth = 15;
+    c.arc(x, y, radius, 0, Math.PI * 3, false);
+    c.strokeStyle = colour;
+    c.lineWidth = 3;
     c.stroke();
 
+
+
+    if (x + radius > innerWidth) {
+        dx = -dx;
+    }
+    if (x - radius < 0) {
+        dx = -dx;
+    }
+
+    if (y + radius > innerHeight) {
+        dy = -dy;
+    }
+    if (y - radius < 0) {
+        dy = -dy;
+    }
+
+    x += dx;
+    y += dy;
+
+    requestAnimationFrame(animate);
 }
 
-function make_random_colour(){
-    var randomColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+
+animate();
+
+function make_random_colour() {
+    var randomColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
     // console.log(randomColor);
     return randomColor;
 
